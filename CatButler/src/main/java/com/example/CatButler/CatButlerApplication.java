7ca9bd6ed487d6da50a1a9b76ab90f77f8320a6a
volkinuna <plleiades@gmail.com>
2024-04-09 14:02:36 +0900

    start

---
+++ CatButler/src/main/java/com/example/CatButler/CatButlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatButler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatButlerApplication.class, args);
+	}
+
+}
---
+++ CatButler/src/test/java/com/example/CatButler/CatButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatButler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatButlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
